32e01f5a93e56c6402ee52270cd0cfe8556e88f1
Ольга <koval.oi.1106@gmail.com>
2022-09-08 08:33:22 +0300

    1 коммит

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+                int [] arr = generateRandomArray ();
+                public static int [] generateRandomArray () {
+                    java.util.Random random = new java.util.Random();
+                    int [] arr = new int [30];
+            for (int i = 0; i < arr.length; i++) {
+                arr [i] = random.nextInt(100_000) + 100_000;
+
+            }
+            return arr;
+
+        }
+    public static void main(String[] args) {
+
+                    // задание 1
+
+
+    }
+}
